9d379ce4e1e66ffe60cf63354bb1883fbe94df41
Mohan0405 <61442843+Mohan0405@users.noreply.github.com>
2025-06-17 20:55:02 +0000

    12

--- app.py
+++ app.py
@@ -99,5 +99,9 @@ def answer():
         return jsonify({"error": "Missing 'message' in request."}), 400
     answer = answer_as_chatbot(message)
     return jsonify({"message": answer}), 200
-
+# Initialize chains and run server
+#if __name__ == "__main__":
+ #   qa = load_db()
+  #  chatbot_chain = init_chatbot()
+   # app.run(host="0.0.0.0", port=5000, debug=False)
 
